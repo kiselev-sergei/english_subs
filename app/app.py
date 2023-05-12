6433b7a051cbc162f397d476d9e9889bb7954c24
Sergei Kiselev <sergserg67@ya.ru>
2023-05-12 22:33:05 +0300

    first commit

---
+++ app/app.py
@@ -0,0 +1,53 @@
+import streamlit as st
+from catboost import CatBoostClassifier, Pool
+import pandas as pd
+import nltk
+import time
+import pickle
+from subs_preprocess import sub_processing
+
+model = CatBoostClassifier()
+features = ['Subtitles']
+def load():
+    with open('./sub_model.pcl', 'rb') as mod:
+        return pickle.load(mod)
+model = load()
+
+TITLE = 'English level prediction with film subtitles'
+st.set_page_config(
+                   page_title=TITLE,
+                   page_icon='🎬',
+                   initial_sidebar_state='expanded',
+                 )
+st.title(TITLE)
+st.write('This ML-based app predicts the linguistic level of a film for English learners. The classification is based on CEFR levels (A1, A2, B1, B2, C1, C2). Upload susbtitles in .srt format to know the level.')
+
+upload_file = st.file_uploader('Upload subtitles in .srt format', type='srt')
+
+def make_predict(data, model):
+    """
+    :param data:
+    :param model:
+    :return:
+    """
+    predict_pool = Pool(data=data,
+                       )
+    predict = model.predict(predict_pool)
+    decode = {1:'A1',
+              2:'A2',
+              3:'B1',
+              4:'B2',
+              5:'C1',
+              6:'C2'
+             }
+    return predict
+
+if upload_file:
+
+    print(upload_file.name)
+
+    df = sub_processing(upload_file)
+    if df is None:
+        st.write('Problem with the subs file. Try another one')
+    else:
+        st.header(f'This film is labeled **:[{make_predict(df, model)}]** on CEFR classification')
---
+++ app/subs_preprocess.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import re
+import numpy as np
+import pysrt
+import spacy
+import en_core_web_sm
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfTransformer
+import nltk
+nltk.download('stopwords')
+from nltk.corpus import stopwords
+import pickle
+
+def load():
+    with open('./sub_pipe.pcl', "rb") as f:
+        return pickle.load(f)
+pipe = load()
+
+nlp = spacy.load("en_core_web_sm")
+
+def sub_processing(upload_file):
+    try:
+        subs = pysrt.from_string(upload_file.getvalue().decode('cp1252'))
+        print('Decode ANSI success')
+        if subs.text == '':
+            subs = pysrt.from_string(upload_file.getvalue().decode('utf-16'))
+            print('Decode UTF-16 success')
+        print('Read file success')
+    except Exception as ex:
+        template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+        message = template.format(type(ex).__name__, ex.args)
+        print(message)
+        return
+    
+    # Очистка текста
+    text = re.sub('<i>|</i>', '', subs.text)
+    text = re.sub('\<.*?\>', '', text)      # удаляем то что в скобках <>
+    text = re.sub('\n', ' ', text)               # удаляем разделители строк
+    # text = re.sub('<font.*?font>', '', text)
+    text = re.sub('\(.*?\)', '', text)           # удаляем то что в скобках ()
+    text = re.sub('\[.*?\]', '', text)           # удаляем то что в скобках []
+    text = re.sub('[A-Z]+?:', '', text)          # удаляем слова написанные заглавными буквами с двоеточием(это имена тех кто говорит)
+    text = re.sub('\.+?:', '\.', text)           # Заменяем троеточия на одну точку
+    text = text.lower()
+    text = re.sub('[^a-z\.\!\?]', ' ', text)     # удаляем всё что не буквы и не .?!
+    text = re.sub(' +', ' ', text)               # удаляем " +"
+    spacy_results = nlp(text)
+    text = ' '.join([token.lemma_ for token in spacy_results])
+    text = [text]
+    text = pipe.transform(text).toarray()
+    return text
